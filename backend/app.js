5d3f51063c1c71454e9e035d1afa8482713c0733
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2023-05-18 10:41:56 +0530

    User_Auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import express from "express";
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/userRoute.js";
 import { errorMiddleware } from "./middleware/error.js";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
@@ -8,23 +8,26 @@ import morgan from "morgan";
 import { logRequests } from "./middleware/logreqs.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser";
 export const app = express();
 
 const swaggerDocument = YAML.load("./swagger.yaml");
-app.use(logRequests);
-app.use(cookieParser());
-app.use(morgan("dev"));
 config({
   path: "./data/config.env",
 });
 //Using Middleware
+app.use(fileUpload());
+app.use(logRequests);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 //swaggerUI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //Using routes
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1", userRouter);
 
 //Using Error Middileware
 app.use(errorMiddleware);
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,19 @@
 import { User } from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import ErrorHandler from "./error.js";
 
 export const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    return res.status(404).json({
-      success: false,
-      message: "Login First",
-    });
+    return next(new ErrorHandler("Please Login to access this resource", 401))
+    //  res.status(404).json({
+    //   success: false,
+    //   message: "Login First",
+    // });
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 //   console.log(decoded, "tok-decode");
   req.user = await User.findById(decoded._id);
   next();
 };
+
